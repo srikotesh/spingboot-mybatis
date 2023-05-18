b4103d9e40861730349675318758215b217e18fc
srikotesh <srikotesh@gmail.com>
2023-05-18 22:41:17 +0530

    sprintbott-mybatis starter

---
+++ springboot-mybatis/src/main/java/com/example/springbootmybatis/SpringbootMybatiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMybatiApplication.class, args);
+	}
+
+}
